a6f1eba9cbdad29105af2e3a8634a3a799104dea
onedavid <davi16lins@gmail.com>
2024-03-11 21:35:20 -0300

    new changes

---
+++ ListaExercicios/Exe1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+   static void Main()
+   {
+      // Criando um vetor de inteiros
+      int[] vetor = { 8, 9, 10, 14, 27 };
+
+      // Ordenando o vetor
+      Array.Sort(vetor);
+
+      // Exibindo o vetor ordenado
+      Console.WriteLine("Vetor ordenado:");
+      foreach (int numero in vetor)
+      {
+         Console.Write(numero + " ");
+      }
+
+      // Aguarda o usuário pressionar uma tecla antes de fechar o console
+      Console.ReadKey();
+   }
+}
